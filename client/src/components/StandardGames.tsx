a40bae9ac90527f657b5a63d63209af05f0c746a
MaRcR11 <MaRcR11@users.noreply.github.com>
2024-01-19 13:36:12 +0000

    Prettified Code!

--- client/src/components/StandardGames.tsx
+++ client/src/components/StandardGames.tsx
@@ -32,7 +32,7 @@ function StandardGames(props: StandardGamesProps) {
   const [multiplier, setMultiplier] = useState<number>(1);
   const [previousPlayerStats, setPreviousPlayerStats] = useState<PlayerStats | Record<string, never>>({});
   const [playerStats, setPlayerStats] = useState<PlayerToPlayerStats>(() =>
-    initializePlayerStats(props.players, props.gamemodeTotalScore)
+    initializePlayerStats(props.players, props.gamemodeTotalScore),
   );
 
   const handleScoreChange = (points: number): void => {
