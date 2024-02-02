8815f216d2e4279229940aaf94e71a9eb89a0345
Simon Nikolaidis <s-nikolaidis@web.de>
2024-02-02 16:20:06 +0100

    sini135# +Impressum, *Type errors fixed

---
+++ client/src/pages/information/Impressum.tsx
@@ -0,0 +1,29 @@
+function Impressum() {
+  return (
+    <div className="hero is-justify-content-center is-align-items-center is-fullheight">
+      <div className="hero-body">
+        <div className="content">
+          <title>Impressum</title>
+          <h1>Impressum</h1>
+          <hr style={{ backgroundColor: "black" }}></hr>
+          <p>
+            <strong>Address:</strong>
+            <br />
+            Felix Herpich
+            <br />
+            Blochstra&szlig;e 1<br />
+            04329 Leipzig
+          </p>
+          <p>
+            <strong>Phone:</strong> +49 341 25687134
+          </p>
+          <p>
+            <strong>E-Mail:</strong> s5002080@ba-sachsen.de
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Impressum;
