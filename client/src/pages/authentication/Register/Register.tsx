1097a1721be6dc128ea0b220de9b16141e61aa20
MaRcR11 <marcr.11@t-online.de>
2024-02-08 17:11:39 +0100

    now users have to repeat the pwd

--- client/src/pages/authentication/Register/Register.tsx
+++ client/src/pages/authentication/Register/Register.tsx
@@ -14,6 +14,8 @@ function Register(props: RegisterProps) {
   const [isPwdDisabled, setPwdDisabled] = useState(false);
   const userIDRef = useRef<HTMLInputElement | null>(null);
   const userMailRef = useRef<HTMLInputElement | null>(null);
+  const pwdRef = useRef<HTMLInputElement | null>(null);
+  const repeatPwdRef = useRef<HTMLInputElement | null>(null);
   const [policyAccepted, setPolicyAccepted] = useState<boolean>(Boolean(localStorage.getItem("privacyPolicyAccepted")));
   const navigate = useNavigate();
 
@@ -23,8 +25,15 @@ function Register(props: RegisterProps) {
       invalidPwdMsgRef.current!.style.display = "block";
       return;
     }
-    if (!props.pwdRef.current!.value || !userIDRef.current!.value) return;
-    const userPWD = props.pwdRef.current!.value;
+    const userPWD = pwdRef.current!.value;
+    const repeatPWD = repeatPwdRef.current!.value;
+    if (userPWD !== repeatPWD) {
+      props.setLoginErrorMsg("Passwords do not match");
+      invalidPwdMsgRef.current!.style.display = "block";
+      return;
+    }
+    if (!userPWD || !userIDRef.current!.value || !userMailRef.current!.value) return;
+
     const userID = userIDRef.current!.value;
     const userMail = userMailRef.current!.value;
 
@@ -58,91 +67,100 @@ function Register(props: RegisterProps) {
 
   return (
     <>
-      {
-        <div className="hero is-fullheight ">
-          {isPwdDisabled && <BarLoader id="top-barloader" color={"#00d1b2"} width={"100%"} />}
-          <div className="hero-body  is-justify-content-center is-align-items-center">
-            <div className="columns is-half is-flex is-flex-direction-column box">
-              <div className="column is-flex is-justify-content-center">
-                <h1 className="is-size-5">Register</h1>
-              </div>
-              <div className="column">
-                <input
-                  ref={userMailRef}
-                  disabled={isPwdDisabled}
-                  autoFocus
-                  onChange={onChangeHideInvalidPwdMsg}
-                  onKeyUp={onEnterPressed}
-                  className="input is-primary"
-                  placeholder="e-mail"
-                  type="email"
-                />
-              </div>
-              <div className="column">
-                <input
-                  ref={userIDRef}
-                  disabled={isPwdDisabled}
-                  onChange={onChangeHideInvalidPwdMsg}
-                  onKeyUp={onEnterPressed}
-                  className="input is-primary"
-                  placeholder="username"
-                />
-              </div>
-              <div className="column">
-                <input
-                  id="pwdInput"
-                  ref={props.pwdRef}
-                  disabled={isPwdDisabled}
-                  onChange={onChangeHideInvalidPwdMsg}
-                  onKeyUp={onEnterPressed}
-                  className="input is-primary"
-                  type="password"
-                  placeholder="password"
-                />
-                <p ref={invalidPwdMsgRef} style={{ display: "none" }} className="help is-danger">
-                  {props.loginErrorMsg}
-                </p>
-              </div>
-              <div className="column policy pt-0">
-                <input
-                  className="checkbox mr-2"
-                  type="checkbox"
-                  defaultChecked={policyAccepted}
-                  onChange={() => {
-                    setPolicyAccepted(!policyAccepted);
-                  }}
-                  style={{ verticalAlign: "bottom", height: "100%" }}
-                />
-                I agree to the{" "}
-                <a className="policyLink" href="client/src/components" target="_blank">
-                  <Link to={"/privacy-policy"}>privacy policy</Link>
+      <div className="hero is-fullheight">
+        {isPwdDisabled && <BarLoader id="top-barloader" color={"#00d1b2"} width={"100%"} />}
+        <div className="hero-body is-justify-content-center is-align-items-center">
+          <div className="columns is-half is-flex is-flex-direction-column box">
+            <div className="column is-flex is-justify-content-center">
+              <h1 className="is-size-5">Register</h1>
+            </div>
+            <div className="column">
+              <input
+                ref={userMailRef}
+                disabled={isPwdDisabled}
+                autoFocus
+                onChange={onChangeHideInvalidPwdMsg}
+                onKeyUp={onEnterPressed}
+                className="input is-primary"
+                placeholder="e-mail"
+                type="email"
+              />
+            </div>
+            <div className="column">
+              <input
+                ref={userIDRef}
+                disabled={isPwdDisabled}
+                onChange={onChangeHideInvalidPwdMsg}
+                onKeyUp={onEnterPressed}
+                className="input is-primary"
+                placeholder="username"
+              />
+            </div>
+            <div className="column">
+              <input
+                id="pwdInput"
+                ref={pwdRef}
+                disabled={isPwdDisabled}
+                onChange={onChangeHideInvalidPwdMsg}
+                onKeyUp={onEnterPressed}
+                className="input is-primary"
+                type="password"
+                placeholder="password"
+              />
+            </div>
+            <div className="column">
+              <input
+                ref={repeatPwdRef}
+                disabled={isPwdDisabled}
+                onChange={onChangeHideInvalidPwdMsg}
+                onKeyUp={onEnterPressed}
+                className="input is-primary"
+                type="password"
+                placeholder="repeat password"
+              />
+              <p ref={invalidPwdMsgRef} style={{ display: "none" }} className="help is-danger">
+                {props.loginErrorMsg}
+              </p>
+            </div>
+            <div className="column policy pt-0">
+              <input
+                className="checkbox mr-2"
+                type="checkbox"
+                defaultChecked={policyAccepted}
+                onChange={() => {
+                  setPolicyAccepted(!policyAccepted);
+                }}
+                style={{ verticalAlign: "bottom", height: "100%" }}
+              />
+              I agree to the{" "}
+              <a className="policyLink" href="client/src/components" target="_blank">
+                <Link to={"/privacy-policy"}>privacy policy</Link>
+              </a>
+            </div>
+            <div className="column">
+              <button
+                onClick={onSubmitPwd}
+                disabled={isPwdDisabled}
+                className="button is-primary is-fullwidth"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+            <div className="has-text-centered">
+              <p className="is-size-7">
+                <span tabIndex={0}>
+                  <Link to={"/login"}>Login</Link>
+                </span>{" "}
+                -&nbsp;
+                <a href="client/src/components" className="has-text-danger">
+                  Help?
                 </a>
-              </div>
-              <div className="column">
-                <button
-                  onClick={onSubmitPwd}
-                  disabled={isPwdDisabled}
-                  className="button is-primary is-fullwidth"
-                  type="submit"
-                >
-                  Register
-                </button>
-              </div>
-              <div className="has-text-centered">
-                <p className="is-size-7">
-                  <span tabIndex={0}>
-                    <Link to={"/login"}>Login</Link>
-                  </span>{" "}
-                  -&nbsp;
-                  <a href="client/src/components" className="has-text-danger">
-                    Help?
-                  </a>
-                </p>
-              </div>
+              </p>
             </div>
           </div>
         </div>
-      }
+      </div>
     </>
   );
 }
