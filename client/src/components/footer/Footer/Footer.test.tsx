ccb51105fc7a9b22b595d2203ddb9f07bebda54d
felikan <felikan@users.noreply.github.com>
2024-02-14 13:30:06 +0000

    Prettified Code!

--- client/src/components/footer/Footer/Footer.test.tsx
+++ client/src/components/footer/Footer/Footer.test.tsx
@@ -1,31 +1,31 @@
 import { render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import Footer from './Footer.tsx';
+import Footer from "./Footer.tsx";
 
 const mockProps = {
-    userID: "exampleUserID",
-    isLoggedIn: true
-  };
+  userID: "exampleUserID",
+  isLoggedIn: true
+};
 
-describe('Footer Component', () => {
+describe("Footer Component", () => {
   it('should render "Impressum" link', () => {
     const { getByText } = render(<Footer {...mockProps} />);
-    const impressumLink = getByText('Impressum');
+    const impressumLink = getByText("Impressum");
     expect(impressumLink).toBeInTheDocument();
-    expect(impressumLink).toHaveClass('mb-3');
-    expect(impressumLink.getAttribute('href')).toBe('/impressum');
-    expect(impressumLink.getAttribute('target')).toBe('_blank');
+    expect(impressumLink).toHaveClass("mb-3");
+    expect(impressumLink.getAttribute("href")).toBe("/impressum");
+    expect(impressumLink.getAttribute("target")).toBe("_blank");
   });
 
-  it('should render user ID if user is logged in', () => {
+  it("should render user ID if user is logged in", () => {
     const { getByText } = render(<Footer {...mockProps} />);
     const userIDText = getByText(`Logged in as ${mockProps.userID}`);
     expect(userIDText).toBeInTheDocument();
     const userIDElement = userIDText.parentElement;
-    expect(userIDElement).toHaveClass('is-flex');
+    expect(userIDElement).toHaveClass("is-flex");
   });
 
-  it('should not render user ID if user is not logged in', () => {
+  it("should not render user ID if user is not logged in", () => {
     const { queryByText } = render(<Footer isLoggedIn={false} userID={mockProps.userID} />);
     const userIDText = queryByText(/Logged in as/i);
     expect(userIDText).toBeNull();
