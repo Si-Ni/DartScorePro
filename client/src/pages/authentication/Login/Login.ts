21f0a3937d48567cdaf4a1c8fd0dcbb30bd11414
Simon Nikolaidis <s-nikolaidis@web.de>
2024-02-21 09:21:14 +0100

    sini299# *tsc fixes

--- client/src/pages/authentication/Login/Login.ts
+++ client/src/pages/authentication/Login/Login.ts
@@ -1,11 +1,9 @@
 import React from "react";
-import { Socket } from "socket.io-client";
 
 export interface LoginProps {
   pwdRef: React.RefObject<HTMLInputElement>;
   loginErrorMsg: string;
   setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
   setDisplayUserID: React.Dispatch<React.SetStateAction<string>>;
-
   setLoginErrorMsg: React.Dispatch<React.SetStateAction<string>>;
 }
